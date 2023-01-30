54f58c0d4ae8086fba03a2e5c053145f7e4f56aa
Nazarov555 <nzrvkeskil@mail.ru>
2023-01-31 02:49:07 +0300

    navbar menu added

--- PRACTICE/1Popup&Navbar/main.js
+++ PRACTICE/1Popup&Navbar/main.js
@@ -1,1 +1,13 @@
-    
+ //navbar script
+ 
+const closeBtn = document.getElementById("close-btn")
+const openBtn = document.getElementById("open-button");
+let navbar = document.querySelector("nav"); 
+
+openBtn.addEventListener('click', () => {
+    navbar.classList.add("open");
+});
+
+closeBtn.addEventListener('click', () =>{
+    navbar.classList.remove("open");
+});
