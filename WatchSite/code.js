fb9f49c2803afb11d5c62c036af83207f6efc6ee
Nazarov555 <nzrvkeskil@mail.ru>
2023-03-29 04:00:55 +0300

    Поманял навбар в сайте в стиле фигма

--- WatchSite/code.js
+++ WatchSite/code.js
@@ -1,5 +1,5 @@
 const menuBtn = document.querySelector('.menu-btn');
-let navbar = document.querySelector('.menu-container');
+let navbar = document.querySelector('.nav-menu');
 
 menuBtn.addEventListener('click', ()=>{
     navbar.classList.toggle('active');
